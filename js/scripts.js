79f55e1c64704e5e3997c860e36c327583f7f969
Mariusz Dabrowski <wayrse@gmail.com>
2018-09-12 17:15:02 -0400

    Added collision detection

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
+// --------------
+// Animation loop
+// --------------
+
 function animationLoop() {
   indicator.updatePosition();
+  indicator.detectCollision();
+  progressBar.updateUi();
+  fish.updateFishPosition();
   requestAnimationFrame(animationLoop);
 }
 
+// ---------
+// Indicator
+// ---------
+
 class Indicator {
   constructor() {
-    this.fishBar = document.querySelector('.game-body');
     this.indicator = document.querySelector('.indicator');
+    this.height = this.indicator.clientHeight;
     this.y = 0;
     this.velocity = 0;
     this.acceleration = 0;
-    this.topBounds = (this.fishBar.clientHeight * -1) + 48;
+    this.topBounds = (gameBody.clientHeight * -1) + 48;
     this.bottomBounds = 0;
   }
 
@@ -39,7 +50,7 @@ class Indicator {
       indicator.y = this.topBounds;
       indicator.velocity = 0;
     } else {
-      if (keyPressed) {
+      if (mouseDown) {
         indicator.applyForce(-0.5);
       }
     }
@@ -50,20 +61,99 @@ class Indicator {
     // Update object position
     this.indicator.style.transform = `translateY(${this.y}px)`;
   }
+
+  detectCollision() {
+    if (
+      fish.y < this.y && fish.y > this.y - this.height ||
+      fish.y - fish.height < this.y && fish.y - fish.height > this.y - this.height
+    ) {
+      console.log('collision'); 
+    }
+  }
 }
 
-let keyPressed = false;
+// ----
+// Fish
+// ----
+
+class Fish {
+  constructor() {
+    this.fish = document.querySelector('.fish');
+    this.height = this.fish.clientHeight;
+    this.y = 0;
+    this.direction = 'up';
+  }
+
+  updateFishPosition() {
+    if (this.direction === 'down') {
+      this.y += 0.1;
+      if (this.y >= 0) this.direction = 'up';
+    } else {
+      this.y -= 0.1;
+      if (this.y - this.fish.clientHeight < gameBody.clientHeight * -1) this.direction = 'down';
+    }
+    
+    this.fish.style.transform = `translateY(${this.y}px)`;
+  }
+}
+
+// ------------
+// Progress bar
+// ------------
+
+class ProgressBar {
+  constructor() {
+    this.wrapper = document.querySelector('.progress-bar');
+    this.progressBar = this.wrapper.querySelector('.progress-gradient-wrapper');
+    this.progress = 100;
+  }
+
+  drain() {
+    if (this.progress > 0) this.progress -= 0.5;
+  }
+
+  fill() {
+    if (this.progress < 100) this.progress += 0.5;
+  }
+
+  updateUi() {
+    if (mouseDown) {
+      this.fill();
+    } else {
+      this.drain();
+    }
+
+    this.progressBar.style.height = `${this.progress}%`
+  }
+}
+
+// -----------
+// Application
+// -----------
+
+const gameBody = document.querySelector('.game-body');
+let mouseDown = false;
 const indicator = new Indicator();
+const progressBar = new ProgressBar();
+const fish = new Fish();
 
-window.addEventListener('keydown', keyDown);
-window.addEventListener('keyup', keyUp);
+// ------------
+// Mouse events
+// ------------
 
-function keyDown() {
-  keyPressed = true;
+window.addEventListener('mousedown', mouseDownFunc);
+window.addEventListener('mouseup', mouseUpFunc);
+
+function mouseDownFunc() {
+  mouseDown = true;
 }
 
-function keyUp() {
-  keyPressed = false;
+function mouseUpFunc() {
+  mouseDown = false;
 }
+
+// -------------
+// Initiate loop
+// -------------
 
 animationLoop();
