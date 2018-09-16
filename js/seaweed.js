203fdbaf42c9b95751b460bbf7caa211f709a218
Mariusz Dabrowski <wayrse@gmail.com>
2018-09-16 13:21:18 -0400

    seaweed with spikes

--- js/seaweed.js
+++ js/seaweed.js
@@ -17,35 +17,51 @@
   }
 
   class Seaweed {
-    constructor() {
-      this.segments = 4;
-      this.segmentSpread = 20;
-      this.x = canvas.width/2;
-      this.y = canvas.height/2 - 50;
+    constructor(segments, spread, xoff) {
+      this.segments = segments;
+      this.segmentSpread = spread;
+      this.x = 0;
+      this.xoff = xoff;
+      this.y = 0;
       this.radius = 1;
-      this.sin = 0;
+      this.sin = Math.random() * 10;
     }
 
     draw() {
       context.beginPath();
-      context.strokeStyle="red";
+      context.strokeStyle="#143e5a";
+      context.fillStyle="#143e5a";
       context.lineWidth=2;
-      for (let i = 1; i <= this.segments; i++) {
-        if (i === 1) {
-          context.moveTo(Math.sin(this.sin + i) * 10 + 30, this.y + (this.segmentSpread * i));
+      for (let i = this.segments; i >= 0; i--) {
+        context.arc(
+          Math.sin(this.sin + i) * i/2.5 + this.xoff + 0.5,
+          canvas.height + (-i * this.segmentSpread),
+          0.2 * Math.abs(i - this.segments),
+          0,
+          2*Math.PI
+        );
+        if (i === this.segments) {
+          context.moveTo(
+            Math.sin(this.sin + i) * i/2.5 + this.xoff,
+            canvas.height + (-i * this.segmentSpread),
+          );
         } else {
-          context.lineTo(Math.sin(this.sin + i) * 10 + 30, this.y + (this.segmentSpread * i));
+          context.lineTo(
+            Math.sin(this.sin + i) * i/2.5 + this.xoff,
+            canvas.height + (-i * this.segmentSpread),
+          );
         }
         // context.arc(Math.sin(this.sin + i) * 10 + 30, this.y + (this.segmentSpread * i), this.radius, 0, 2*Math.PI); 
       }
-      
       context.stroke();
 
-      this.sin += 0.1;
+      this.sin += 0.05;
     }
   }
 
-  seaweed.push(new Seaweed());
+  seaweed.push(new Seaweed(6, 8, 25));
+  seaweed.push(new Seaweed(8, 10, 35));
+  seaweed.push(new Seaweed(4, 8, 45));
 
   animationLoop()
 })();
